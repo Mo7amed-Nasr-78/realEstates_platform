735cd6e1976c57f57bc08c2fb44e2ff1166dd51a
mohamed_nasr <mohamed.ibrahim.nasr2020@gmail.com>
2025-09-11 16:08:26 +0300

    some improvements

--- Frontend/src/components/Modal.jsx
+++ Frontend/src/components/Modal.jsx
@@ -2,7 +2,7 @@ function Modal({ children }) {
     return (
         <div className="fixed top-0 left-0 w-screen h-screen flex items-center justify-center bg-[rgb(41,44,51)]/80 z-50 px-4 sm:px-0">
             <div className="relative w-full h-11/12 max-h-11/12 flex flex-col items-center sm:justify-center overflow-y-scroll scrollbar-none">
-                <div className="modal border border-(--secondary-text) rounded-3xl">
+                <div className="modal border border-(--secondary-text) rounded-3xl my-auto">
                     { children }
                 </div>
             </div>
